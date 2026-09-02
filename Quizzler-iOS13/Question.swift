69a545e67160a8d53e249184f51ff520ea339a8a
EthanGunawan <gunawane@sheridancollege.ca>
2026-09-02 10:31:31 -0400

    Created Quiz Struct and Bind Data

---
+++ Quizzler-iOS13/Question.swift
@@ -0,0 +1,15 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Ethan J on 2026-09-01.
+//  Copyright © 2026 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+}
--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz = [Question(text: "9 + 9 = 2" , answer: "False"),
+                Question(text: "8 + 2 = 10" , answer: "True"),
+                Question(text: "Dolphins are sea mammals", answer: "True")]
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+      updateUI()
+        
     }
-
-
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle
+        let actualAnswer = quiz[questionNumber].answer
+        
+        if userAnswer == actualAnswer{
+            print("Correct!")
+        }else{
+            print("Wrong!")
+        }
+        if questionNumber+1<quiz.count{
+            questionNumber+=1
+        }
+        else{
+            questionNumber = 0
+        }
+        updateUI()
+        
+    }
+    
+    func updateUI(){
+        questionLabel.text = quiz[questionNumber].text
+    }
 }
 
